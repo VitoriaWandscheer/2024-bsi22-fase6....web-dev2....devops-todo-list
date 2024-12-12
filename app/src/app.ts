dfcc62f57fba7a8ed8d1902b97067dfe73e57b08
Vitória <vitoriawpereira2404@gmail.com>
2024-12-11 23:08:23 -0300

    First commit

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,6 +1,42 @@
-import express from "express"
-import router from "./routes"
-const app = express()
-const PORT = 3000
-app.use(router)
-app.listen(PORT, () => console.log(`⚡server is running on port: ${PORT}`))
+import express from "express" // Importa o Express para criar o servidor da aplicação web
+import router from "./routes" // Importa o arquivo router, que contém as rotas definidas
+import { Pool, Client } from 'pg'; // // Importa a classe Pool do 'pg' para criar um pool de conexões com o banco de dados
+
+const app = express() // Cria instância do express
+const PORT = 8080 // Define a porta
+
+const pool = new Pool({
+    connectionString: "postgres://admin:123123@localhost:5432/primary_db"
+});
+
+// const poolSecondary = new Pool({
+//     connectionString: process.env.POSTGRES_SECONDARY_URL
+// });
+
+// const poolTertiary = new Pool({
+//     connectionString: process.env.POSTGRES_TERTIARY_URL
+// });
+
+async function queryDatabase(pool:any, query:any, serverName:any) {
+    const client = await pool.connect();
+    try {
+
+        const res = await client.query(query);
+        console.log(`Servidor ${serverName}:`, res.rows[0].message);
+    } catch (err) {
+        console.error(err);
+    } finally {
+        client.release();
+    }
+}
+
+async function main() {
+    await queryDatabase(pool, 'SELECT $1::text as message', 'Principal');
+    // await queryDatabase(poolSecondary, 'SELECT $1::text as message', 'Secundário');
+    // await queryDatabase(poolTertiary, 'SELECT $1::text as message', 'Terciário');
+}
+
+main();
+
+app.use(router) // Usa o router para lidar com as requisições
+app.listen(PORT, () => console.log(`⚡server is running on port: ${PORT}`)) // Inicia o servidor e printa a porta que está em uso.
--- app/src/routes/index.ts
+++ app/src/routes/index.ts
@@ -2,6 +2,6 @@ import { Router, json } from "express"
 import userRouter from "./user"
 const router = Router()
 router.use(json())
-router.get("/", (req, res) => { res.send("Olá mundo cruel!") })
+router.get("/", (req, res) => { res.send("Bom dia!") })
 router.use("/user", userRouter)
 export default router
